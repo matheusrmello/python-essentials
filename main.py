edf9596b52447e56255b31c6c556fe9b521d29b4
matheusrmello <matheus.rm.09@hotmail.com>
2023-09-01 17:39:27 -0300

    Implementamos o Hello World Multi Linguas

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python3
-print("Calculadora de media")
-nota1 = int(input("Digite a primeira nota: "))
-nota2 = int(input("Digite a segunda nota: "))
-media = (nota1 + nota2) / 2
-print("Sua media eh:", media)
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente
+ o programa exibe a mensagem correspondente
+
+ Como usar:
+ Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_br
+
+Execucao:
+    python3 main.py
+"""
+__version__ = "0.0.1"
+__author__ = "Matheus Mello"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello World"
+
+if current_language == "pt_BR":
+    msg = "Ola, mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+print(msg)
+
 
 
 
