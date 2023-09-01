d6bd2fbd3c79e56b3b1cd50f3a29a8ecf4332f2e
matheusrmello <matheus.rm.09@hotmai.com>
2023-09-01 16:35:26 -0300

    Alterando projeto

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print("Hello world")
+#!/usr/bin/env python3
+print("Calculadora de media")
 nota1 = int(input("Digite a primeira nota: "))
 nota2 = int(input("Digite a segunda nota: "))
 media = (nota1 + nota2) / 2
-print(media)
+print("Sua media eh:", media)
 
 
 
